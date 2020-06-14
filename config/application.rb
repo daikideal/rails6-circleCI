92ee957564bcd79c700bc2be4068e007fc020a4e
daikideal <54879050+daikideal@users.noreply.github.com>
2020-06-15 00:47:15 +0900

    Clean: rubocopでコードを整形
    circleCIで引っかからないように整形してみた。

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
 # require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
 # require "action_cable/engine"
-require "sprockets/railtie"
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -34,12 +36,12 @@ module Rails6Circleci
       g.skip_routes true
       g.helper false
       g.test_flamework :rspec,
-        controller_specs: false,
-        fixtures: true,
-        helper_specs: false,
-        request_specs: false,
-        routing_specs: false,
-        view_specs: false
+                       controller_specs: false,
+                       fixtures: true,
+                       helper_specs: false,
+                       request_specs: false,
+                       routing_specs: false,
+                       view_specs: false
       g.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
 
